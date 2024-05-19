817371c241a022e1ff3242bc256fa1ccb3a5a421
NinaMwangi <n.mwangi@alustudent.com>
2024-05-19 13:59:02 -0700

    Pycodestyle aligning

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -9,7 +9,7 @@ def add_arrays(arr1, arr2):
         The function that adds two arrays element wise.
         arr1: The first array
         arr2: The second array
-        Returns: A list containing sums of arr1 & arr2. 
+        Returns: A list containing sums of arr1 & arr2.
         If not same shape returns none.
     '''
     if len(arr1) != len(arr2):
