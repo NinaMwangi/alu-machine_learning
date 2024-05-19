b4611e4ee2f77618070c5abb1bf321522e8fa8b0
NinaMwangi <n.mwangi@alustudent.com>
2024-05-19 12:32:26 -0700

    Pycodestyle correction

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
 ''' 
     This script contains a function that concatenates two arrays.
 '''
+
+
 def cat_arrays(arr1, arr2):
     ''' This is the function that will concatenate 2 arrays.
         arr1: First array
