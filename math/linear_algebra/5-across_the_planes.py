a991e1c32b7f7265ad5e0a5e5f0e830eec1a8e5a
NinaMwangi <n.mwangi@alustudent.com>
2024-05-19 12:18:46 -0700

    Pycodestyle correction

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,12 +4,14 @@
     This script contains a function that adds matrices element wise.
 '''
 
+
 def add_matrices2D(mat1, mat2):
     '''
         This function adds matrices element wise.
         mat1: The first matrix
         mat2: The second matrix
-        Returns: A new matrix and None if the matrices are not of the same shape.
+        Returns: A new matrix and 
+        None if the matrices are not of the same shape.
     '''
     if len(mat1) != len(mat2):
         return None
@@ -19,4 +21,5 @@ def add_matrices2D(mat1, mat2):
             return None
 
 # Creating a new matrix with the element wise sums.
-    return[[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    return[[a + b for a, b in zip(row1, row2)] 
+    for row1, row2 in zip(mat1, mat2)]
