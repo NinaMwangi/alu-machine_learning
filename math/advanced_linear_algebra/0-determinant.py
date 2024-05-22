eebe9a3ede5dc730856bd095394e9dd8df79b627
NinaMwangi <n.mwangi@alustudent.com>
2024-05-22 06:24:10 -0700

    File 1

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+    This script calculates the determinant of a matrix.
+'''
+
+def determinant(matrix):
+    '''
+        The function that will calculate the determinant.
+    '''
