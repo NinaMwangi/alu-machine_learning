9cf3677128b496084fa79d754f62903713e5a87c
NinaMwangi <n.mwangi@alustudent.com>
2024-07-03 15:00:27 +0300

    Task 2

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -35,7 +35,7 @@ def convolve_grayscale_same(images, kernel):
     c_height = images.shape[1]
     c_width = images.shape[2]
 
-    pad_images = np.pad(images ((0, 0), (padding_h, padding_h), (padding_w, padding_w)))
+    pad_images = np.pad(images, ((0, 0), (padding_h, padding_h), (padding_w, padding_w)))
     convolved = np.zeros(c_images, c_height, c_width)
     for row in range(c_height):
         for col in range(c_width):
