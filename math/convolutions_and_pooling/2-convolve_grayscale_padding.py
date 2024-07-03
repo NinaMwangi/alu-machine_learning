42ae0fc0a7163d0d09c496380ba991cf1f89f5c9
NinaMwangi <n.mwangi@alustudent.com>
2024-07-03 15:50:13 +0300

    corrections

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -28,7 +28,7 @@ def convolve_grayscale_padding(images, kernel, padding):
     c_height = images.shape[1] + 2 * padding_h - f_height + 1
     c_width = images.shape[2] + 2 * padding_w - f_width + 1
 
-    pad_images = np.pad(images, ((0, 0) (padding_h, padding_h), (padding_w,
+    pad_images = np.pad(images, ((0, 0), (padding_h, padding_h), (padding_w,
                                                                  padding_w)))
     convolved = np.zeros((c_images, c_height, c_width))
     for row in range(c_height):
